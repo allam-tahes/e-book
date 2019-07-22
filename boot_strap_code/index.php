7f5785eea960a0a2330d724b0bd4be62441d0783
allam-tahes <allam4cs@gmail.com>
2019-07-23 06:56:38 +0700

    first commit

---
+++ boot_strap_code/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Touch SLider - Pure CSS</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+   <section class="section-slider">
+    <div class="slider">
+        <img class="img-slider" src="hal1.png">
+        <img class="img-slider" src="hal2.png">
+        <img class="img-slider" src="hal3.png">
+    </div>
+   </section>
+  
+  
+        <a href="https://www.youtube.com/watch?v=GvQkiiVjkSU&t=315s" class="myvideo" target="_blank">Click Here To View My Video On Youtube</a>
+</body>
+</html>
